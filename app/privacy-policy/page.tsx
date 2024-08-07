49a4c1b1985249d98c45402c1dcced7a9c56ec8e
rahulkumarhavit <rahulkumarhavit@gmail.com>
2024-08-08 00:31:58 +0530

    review

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,80 +1,83 @@
-// export default function privacypolicy() {
-//     return (
-//         <div className="text-gray-400">
-//             <section className="padding">
-//                 <div className="max-container">
-//                     <div className=" mt-10 lg:mt-24 xl:mt-32">
-//                         <div className="text-4xl lg:text-5xl mb-5 xl:mb-5 text-white">Privacy Policy</div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">At Hype, accessible from https://hypecard.in, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Hype and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.
+export default function privacypolicy() {
+    return (
+        // <div className="text-gray-400">
+        //     <section className="padding">
+        //         <div className="max-container">
+        //             <div className=" mt-10 lg:mt-24 xl:mt-32">
+        //                 <div className="text-4xl lg:text-5xl mb-5 xl:mb-5 text-white">Privacy Policy</div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">At Hype, accessible from https://hypecard.in, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Hype and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.
 
-//                             This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Hype. This policy is not applicable to any information collected offline or via channels other than this website.
-//                         </div>
-//                         <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">Consent
-//                         </div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">By using our website, you hereby consent to our Privacy Policy and agree to its terms.
-//                         </div>
-//                         <div className="text-2xl lg:text-3xl mb-5 xl:mb-5  text-white">Information we collect
-//                         </div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.
-//                         </div>
-//                         <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">How we use your information</div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">
-//                             We use the information we collect in various ways, including to:
-//                             <br /><br />
-//                             1. Provide, operate, and maintain our website
-//                             <br /> <br />
-//                             2.Improve, personalize, and expand our website
-//                             <br /> <br />
-//                             3.Understand and analyze how you use our website
-//                             <br /> <br />
-//                             4. Develop new products, services, features, and functionality
-//                             <br /> <br />
-//                             5. Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes
-//                             <br /> <br />
-//                             6. Send you emails
-//                             <br /> <br />
-//                             7.Find and prevent fraud
-//                         </div>
-//                         <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">Log Files </div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">
-//                             Hype follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
-//                         </div>
-//                         <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">Advertising Partners Privacy Policies</div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">
-//                             You may consult this list to find the Privacy Policy for each of the advertising partners of Hype.
-//                             <br /> <br />
-//                             Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Hype, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
-//                             <br /> <br />
-//                             Note that Hype has no access to or control over these cookies that are used by third-party advertisers.
-//                         </div>
-//                         <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">
-//                             Third Party Privacy Policies
-//                         </div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">
-//                             Hype's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
-//                             <br /> <br />
-//                             You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
-//                         </div>
-//                         <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white"> CCPA Privacy Rights (Do Not Sell My Personal Information) </div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">
-//                             Under the CCPA, among other rights, California consumers have the right to:
-//                             <br /> <br />
-//                             Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
-//                             <br /> <br />
-//                             Request that a business delete any personal data about the consumer that a business has collected.
-//                             <br /> <br />
-//                             Request that a business that sells a consumer's personal data, not sell the consumer's personal data.
-//                             <br /> <br />
-//                             If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
-//                         </div>
-//                         <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white"> Children's Information</div>
-//                         <div className="text-base lg:text-lg xl:text-xl mb-5">
-//                             Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
-//                             Hype does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
-//                             </div>
-//                     </div>
-//                 </div>
-//             </section>
-//         </div>
-//     )
-// }
+        //                     This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Hype. This policy is not applicable to any information collected offline or via channels other than this website.
+        //                 </div>
+        //                 <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">Consent
+        //                 </div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">By using our website, you hereby consent to our Privacy Policy and agree to its terms.
+        //                 </div>
+        //                 <div className="text-2xl lg:text-3xl mb-5 xl:mb-5  text-white">Information we collect
+        //                 </div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.
+        //                 </div>
+        //                 <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">How we use your information</div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">
+        //                     We use the information we collect in various ways, including to:
+        //                     <br /><br />
+        //                     1. Provide, operate, and maintain our website
+        //                     <br /> <br />
+        //                     2.Improve, personalize, and expand our website
+        //                     <br /> <br />
+        //                     3.Understand and analyze how you use our website
+        //                     <br /> <br />
+        //                     4. Develop new products, services, features, and functionality
+        //                     <br /> <br />
+        //                     5. Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes
+        //                     <br /> <br />
+        //                     6. Send you emails
+        //                     <br /> <br />
+        //                     7.Find and prevent fraud
+        //                 </div>
+        //                 <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">Log Files </div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">
+        //                     Hype follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+        //                 </div>
+        //                 <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">Advertising Partners Privacy Policies</div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">
+        //                     You may consult this list to find the Privacy Policy for each of the advertising partners of Hype.
+        //                     <br /> <br />
+        //                     Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Hype, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+        //                     <br /> <br />
+        //                     Note that Hype has no access to or control over these cookies that are used by third-party advertisers.
+        //                 </div>
+        //                 <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white">
+        //                     Third Party Privacy Policies
+        //                 </div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">
+        //                     Hype's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
+        //                     <br /> <br />
+        //                     You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
+        //                 </div>
+        //                 <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white"> CCPA Privacy Rights (Do Not Sell My Personal Information) </div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">
+        //                     Under the CCPA, among other rights, California consumers have the right to:
+        //                     <br /> <br />
+        //                     Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
+        //                     <br /> <br />
+        //                     Request that a business delete any personal data about the consumer that a business has collected.
+        //                     <br /> <br />
+        //                     Request that a business that sells a consumer's personal data, not sell the consumer's personal data.
+        //                     <br /> <br />
+        //                     If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+        //                 </div>
+        //                 <div className="text-2xl lg:text-3xl mb-5 xl:mb-5 text-white"> Children's Information</div>
+        //                 <div className="text-base lg:text-lg xl:text-xl mb-5">
+        //                     Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
+        //                     Hype does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
+        //                     </div>
+        //             </div>
+        //         </div>
+        //     </section>
+        // </div>
+        <div>
+            
+        </div>
+    )
+}
