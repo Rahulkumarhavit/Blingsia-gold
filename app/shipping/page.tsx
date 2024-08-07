15260034ca82ce23568f81f4445b1ae0a92e8a2b
rahulkumarhavit <rahulkumarhavit@gmail.com>
2024-08-08 00:31:58 +0530

    review

--- app/shipping/page.tsx
+++ app/shipping/page.tsx
@@ -1,34 +1,37 @@
-// export default function shipping() {
-//     return (
-//         <div>
-//             <section className="padding text-gray-400">
-//                 <div className="max-container  mt-10 lg:mt-24 xl:mt-32">
-//                     <div className="text-4xl lg:text-5xl mb-5 xl:mb-5 text-white">Shipping Policy</div>
-//                     <div className="text-base lg:text-lg xl:text-xl mb-5 ml-8">
-//                         For the purposes of making delivery to You, You are required to provide a valid address and/or any other documents/information/ biometric identification that may be specified in this regard by Company and Safe Gold on the Platform from time to time.
-//                         <ol className="list-decimal">
-//                             <li>
-//                                 You are required to take delivery of Your Customer Gold within such maximum period as shall be specified for this purpose by Safe Gold from time to time on the Platform (“Maximum Storage Period”).
-//                             </li>
-//                             <li>
-//                                 In the event that no valid address has been provided by You during the Maximum Storage Period, then Platform and/ Safe Gold shall for a period 1 (one) year commencing from the date of expiry of Maximum Storage Period (such period being the “Grace Period”) attempt at least once to contact You using the contact information provided by You to obtain either.
-//                             </li>
-//                             <ol className="list-decimal ml-5">
-//                                 <li>
-//                                     an address to which You would require the gold in question to be delivered or
+export default function shipping() {
+    return (
+        // <div>
+        //     <section className="padding text-gray-400">
+        //         <div className="max-container  mt-10 lg:mt-24 xl:mt-32">
+        //             <div className="text-4xl lg:text-5xl mb-5 xl:mb-5 text-white">Shipping Policy</div>
+        //             <div className="text-base lg:text-lg xl:text-xl mb-5 ml-8">
+        //                 For the purposes of making delivery to You, You are required to provide a valid address and/or any other documents/information/ biometric identification that may be specified in this regard by Company and Safe Gold on the Platform from time to time.
+        //                 <ol className="list-decimal">
+        //                     <li>
+        //                         You are required to take delivery of Your Customer Gold within such maximum period as shall be specified for this purpose by Safe Gold from time to time on the Platform (“Maximum Storage Period”).
+        //                     </li>
+        //                     <li>
+        //                         In the event that no valid address has been provided by You during the Maximum Storage Period, then Platform and/ Safe Gold shall for a period 1 (one) year commencing from the date of expiry of Maximum Storage Period (such period being the “Grace Period”) attempt at least once to contact You using the contact information provided by You to obtain either.
+        //                     </li>
+        //                     <ol className="list-decimal ml-5">
+        //                         <li>
+        //                             an address to which You would require the gold in question to be delivered or
 
-//                                 </li>
-//                                 <li>
-//                                     Your bank account details into which sale proceeds of the Customer Gold shall be deposited. In the event that Platform and/or Safe Gold has not been able to contact You during the applicable Grace Period using the contact information provided by You or where You shall during the Grace Period either fail to take delivery of the physical gold in question for any reason whosoever (including where You shall not have provided any address to take delivery of such physical gold); or provide the details for a valid bank account into which the proceeds of any sale of such Customer Gold is to be deposited;  then upon expiry of the applicable Grace Period for the Customer Gold in question, Safe Gold shall be entitled to purchase such Customer Gold with the purchase price being the applicable prevailing price displayed on the Platform for purchase of Gold from Customers.
-//                                 </li>
-//                             </ol>
-//                             <li>
-//                                 The purchase proceeds realized from such sale of Gold on Safe Gold’s platform (“Final Sale Proceeds”) after deducting any amounts payable to Safe Gold as storage charges after the free storage period, shall be deposited into a bank account operated by the Trustee Administrator who shall be the sole signatory to such bank account. In the event that You shall during a period of 3 (three) years commencing from the date of expiry of the applicable Grace Period (such period being the “Final Claim Period”) notify either the Platform, Safe Gold and/or the Trustee Administrator that You are claiming the applicable Final Sale Proceeds, the Trustee Administrator shall issue suitable instructions to transfer the Final Sale Proceeds to such bank account as You shall notify for this purpose. Please note that to claim the Final Sale Proceeds You will be required to provide details of a valid bank account and that the Final Sale Proceeds will not be transferred in the absence of such details. At no time will the Final Sale Proceeds be paid to You in cash. In the event that You shall not claim Your Final Sale Proceeds within the Final Claim Period, then the Final Sale Proceeds shall be transferred to the Prime Minister’s Relief Fund or such other fund as You may designate for this purpose at any time prior to the expiry of the Grace Period.
-//                             </li>
-//                         </ol>
-//                     </div>
-//                 </div>
-//             </section>
-//         </div>
-//     )
-// }
+        //                         </li>
+        //                         <li>
+        //                             Your bank account details into which sale proceeds of the Customer Gold shall be deposited. In the event that Platform and/or Safe Gold has not been able to contact You during the applicable Grace Period using the contact information provided by You or where You shall during the Grace Period either fail to take delivery of the physical gold in question for any reason whosoever (including where You shall not have provided any address to take delivery of such physical gold); or provide the details for a valid bank account into which the proceeds of any sale of such Customer Gold is to be deposited;  then upon expiry of the applicable Grace Period for the Customer Gold in question, Safe Gold shall be entitled to purchase such Customer Gold with the purchase price being the applicable prevailing price displayed on the Platform for purchase of Gold from Customers.
+        //                         </li>
+        //                     </ol>
+        //                     <li>
+        //                         The purchase proceeds realized from such sale of Gold on Safe Gold’s platform (“Final Sale Proceeds”) after deducting any amounts payable to Safe Gold as storage charges after the free storage period, shall be deposited into a bank account operated by the Trustee Administrator who shall be the sole signatory to such bank account. In the event that You shall during a period of 3 (three) years commencing from the date of expiry of the applicable Grace Period (such period being the “Final Claim Period”) notify either the Platform, Safe Gold and/or the Trustee Administrator that You are claiming the applicable Final Sale Proceeds, the Trustee Administrator shall issue suitable instructions to transfer the Final Sale Proceeds to such bank account as You shall notify for this purpose. Please note that to claim the Final Sale Proceeds You will be required to provide details of a valid bank account and that the Final Sale Proceeds will not be transferred in the absence of such details. At no time will the Final Sale Proceeds be paid to You in cash. In the event that You shall not claim Your Final Sale Proceeds within the Final Claim Period, then the Final Sale Proceeds shall be transferred to the Prime Minister’s Relief Fund or such other fund as You may designate for this purpose at any time prior to the expiry of the Grace Period.
+        //                     </li>
+        //                 </ol>
+        //             </div>
+        //         </div>
+        //     </section>
+        // </div>
+        <div>
+            
+        </div>
+    )
+}
